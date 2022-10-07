4b079387f51d6fadc7defd271411f9fb29e1ac75
Dani Lucia <danilucia@Danis-MacBook.fios-router.home>
2022-10-07 09:17:37 -0400

    added quotes around redirect url

--- src/spotify.js
+++ src/spotify.js
@@ -3,7 +3,7 @@ const CLIENT_ID = "800d269073764880a14d2efe37ffa2b4";
 const REDIRECT_URI =
   process.env.NODE_ENV === "development"
     ? "http://localhost:3000/"
-    : "https://jmrc1995.github.io/goodsoup/;    // This URI will work for production (same as spotify dashboard)
+    : "https://jmrc1995.github.io/goodsoup/";    // This URI will work for production (same as spotify dashboard)
 const AUTH_ENDPOINT = "http://accounts.spotify.com/authorize";
 const RESPONSE_TYPE = "token";
 
